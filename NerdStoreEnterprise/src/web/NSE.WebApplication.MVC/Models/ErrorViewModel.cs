a86af3b2712cf6b29c77ebd245b3c3c3bbf0c106
Victoralm <Victoralm@users.noreply.github.com>
2023-11-15 10:42:13 -0300

    Setup da arquitetura base

---
+++ NerdStoreEnterprise/src/web/NSE.WebApplication.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NSE.WebApplication.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
