d0df30ae2fda4ac0eb82cf2a619391e46c81dd42
Victoralm <Victoralm@users.noreply.github.com>
2024-11-18 19:33:49 -0300

    Registro e Login de usuário

---
+++ NerdStoreEnterprise/src/services/NSE.Identidade.Api/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [Route("api/identidade")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+        [HttpPost("registrar")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if(!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true,
+                UserName = usuarioRegistro.Email,
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            // Vai checar por "result.Errors" depois
+            return BadRequest();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            // Vai checar por "result.Errors" depois
+            return BadRequest();
+        }
+    }
+}
